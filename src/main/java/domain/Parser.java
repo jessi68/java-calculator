9f9a2122973df3b83f58369571958f450a38623a
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2021-01-02 19:51:28 +0900

    feat: 사용자가 입력받은 문자열을 연산자 기준으로 분리하여 나온 숫자들을 반환하는 함수 구현

--- src/main/java/domain/Parser.java
+++ src/main/java/domain/Parser.java
@@ -1,16 +1,21 @@
 package domain;
 
+import java.util.Arrays;
+
 public class Parser {
+    private static final String OPERATOR_DELIMITER = "\\+|\\-|\\/|\\*";
+
 
     public Parser() {
 
     }
 
-    public int [] parseInteger(String input) {
+    public int [] parseIntegers(String request) {
+
+        return (Arrays.stream(request.split(OPERATOR_DELIMITER))
+                     .mapToInt(number -> Integer.parseInt(number)))
+                     .toArray();
 
     }
 
-    public String [] parseOperator(String input) {
-
-    }
 }
