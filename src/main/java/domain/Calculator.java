583c358b1a488cd2929633edcd961ed34b6ebdd2
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2021-01-03 00:00:16 +0900

    계산기가 주어진 숫자들과 주어진 연산자들을 가지고 계산을 하는 함수 구현

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -1,6 +1,10 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
 public class Calculator {
     public enum Operator {
@@ -24,11 +28,26 @@ public class Calculator {
         }
 
         @Override public String toString() { return this.mark; }
+
+        public static Operator fromMark(String mark) {
+            return Arrays.stream(values())
+                         .findFirst()
+                         .filter(operator -> operator.mark.equals(mark))
+                         .get();
+        }
+
         public abstract int apply(int x, int y);
     }
 
 
-    public static int apply() {
-
+    public static int apply(List<Integer> numbers, List<Operator> operators) {
+        int first, second, result;
+        for(Operator operator: operators) {
+            first = numbers.remove(0);
+            second = numbers.remove(1);
+            result = operator.apply(first, second);
+            numbers.add(0, result);
+        }
+        return numbers.get(0);
     }
 }
